338921afe47b5e025f6af0ec292be8c9e8d260e8
Kevin Cerón <kevin10595@hotmail.com>
2021-06-09 03:13:43 -0600

    Create parameters.php

---
+++ CONFIG/parameters.php
@@ -0,0 +1,18 @@
+<?php
+
+//definicion de parametros (limpiando urls para hacerlas "amigables")
+
+define("base_url", "http://localhost/Practicas/UDEMY/php+qsl/STORE-MVC/");//dominio de proyecto - valor
+
+
+//controlador por defecto
+
+define("controller_default","ProductoController");
+
+
+//Metodo por defecto
+
+define("action_default","index");
+
+
+//Serviran para llamadas a imagenes en vistas,o urls en general en las vistas

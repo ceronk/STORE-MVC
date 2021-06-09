5299b1c98f81c481bb38b01b1b44325dca85cff6
Kevin Cerón <kevin10595@hotmail.com>
2021-06-09 03:14:15 -0600

    Create registro.php

---
+++ VIEWS/USUARIO/registro.php
@@ -0,0 +1,19 @@
+<h1>Registrarse</h1>
+
+
+<form action="index.php?controller=usuario&action=save" method="post">
+
+<label for="nombre">Nombre</label>
+<input type="text" name="nombre" id="" required>
+
+<label for="apellidos">Apellidos</label>
+<input type="text" name="apellidos" id=""required>
+
+<label for="nombre">Email</label>
+<input type="email" name="email" id="" required>
+
+<label for="password">Contraseña</label>
+<input type="password" name="password" id="" required>
+
+<input type="submit" value="Registrarse">
+</form>

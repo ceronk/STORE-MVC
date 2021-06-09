ede9c0a30f4abcfc4315222bae756559a8e5d9e7
Kevin Cerón <kevin10595@hotmail.com>
2021-06-09 03:14:13 -0600

    Create destacados.php

---
+++ VIEWS/PRODUCTO/destacados.php
@@ -0,0 +1,13 @@
+<h1>Productos destacados</h1>
+<div class="product">
+    <img src="assets/img/camiseta.png" alt="">
+    <h2>Camiseta azul </h2>
+    <p>30 dolares</p>
+    <a href="#" class="button">Comprar</a>
+</div>
+<div class="product">
+    <img src="assets/img/camiseta.png" alt="">
+    <h2>Camiseta azul </h2>
+    <p>30 dolares</p>
+    <a href="#" class="button">Comprar</a>
+</div>

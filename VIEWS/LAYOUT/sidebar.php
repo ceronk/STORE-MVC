ae566c0cc12b8fa542d288cd15ac98b6c194312a
Kevin Cerón <kevin10595@hotmail.com>
2021-06-09 03:14:10 -0600

    Create sidebar.php

---
+++ VIEWS/LAYOUT/sidebar.php
@@ -0,0 +1,23 @@
+<!-- BARRA LATERAL -->
+<aside id="lateral">
+    <div id="login" class="block_aside">
+        <h3>Entrar a la web</h3>
+        <form action="#" method="post">
+            <label for="email">Email</label>
+            <input type="text" name="email" id="">
+            <label for="password">Contraseña</label>
+            <input type="password" name="password" id="">
+            <input type="submit" value="Enviar  ">
+        </form>
+        <ul>
+            <li><a href="#">Mis pedidos</a></li>
+            <li><a href="#">Gestionar pedidos</a></li>
+            <li><a href="#">Gestionar categoriass</a></li>
+        </ul>
+    </div>
+</aside>
+
+
+
+<!-- CONTENIDO CENTRAL  -->
+<div id="central">

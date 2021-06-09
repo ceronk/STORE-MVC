f696d93c2130342729a0da30f9f075ecc357f2a8
Kevin Cerón <kevin10595@hotmail.com>
2021-06-09 03:14:07 -0600

    Create header.php

---
+++ VIEWS/LAYOUT/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda de camisetas</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div id="container">
+        <!-- CABECERA -->
+        <header>
+            <div id="logo">
+                <img src="assets/img/camiseta.png" alt="camiseta logo">
+                <a href="index.php">
+                    <h1>Tienda de camisetas</h1>
+                </a>
+            </div>
+        </header>
+        <!-- MENU -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div id="content">

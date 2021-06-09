824287e305d904c4d15489ce9e2902823ec0f818
Kevin Cerón <kevin10595@hotmail.com>
2021-06-09 03:13:55 -0600

    Create ProductoController.php

---
+++ CONTROLLERS/ProductoController.php
@@ -0,0 +1,12 @@
+<?php
+
+class ProductoController{
+    
+    public function index(){
+        
+        
+        //renderizar vista
+
+        require_once 'VIEWS/PRODUCTO/destacados.php';
+    }
+}

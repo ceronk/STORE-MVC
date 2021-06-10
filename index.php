4c905e1f4eef91659fdfd97adba48e65ca49e175
Kevin Cerón <kevin10595@hotmail.com>
2021-06-10 04:33:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,19 @@ require_once 'CONFIG/parameters.php';
 require_once 'VIEWS/LAYOUT/header.php';
 require_once 'VIEWS/LAYOUT/sidebar.php';
 
+function showError()
+{
+    $error = new ErrorController();
+    $error->index();
+}
 
 if (isset($_GET['controller'])) {
     $nombreControlador = $_GET['controller'] . 'Controller';
+} elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+    //carga el controlador por defecto (config/parameters)
+    $nombreControlador = controller_default;
 } else {
-    echo "La pagina que buscas no existe";
+    showError();
     exit();
 }
 
@@ -18,20 +26,21 @@ if (isset($nombreControlador) && class_exists($nombreControlador)) {
 
     $controlador = new $nombreControlador();
 
-    //para llamar vistas de forma dinamica
-    // (recoger variables por get)
+    //llama las vistas de forma dinamica (metodo get)
 
     if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
         $action = $_GET['action'];
-        $controlador->$action(); //de esta forma llama al metodo que tenemos
-        //en esa clase
+        $controlador->$action(); //llamada al metodo dentro de X clase
+    } elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+        //carga la accion por defecto (config/parameters)
+        $actionDefault = action_default;
+        $controlador->$actionDefault();
     } else {
-        echo "La vista que buscas no existe";
+        showError();
     }
 } else {
-    echo "El controlador que buscas no existe";
+    showError();
 }
 
 
 require_once 'VIEWS/LAYOUT/footer.php';
-?>

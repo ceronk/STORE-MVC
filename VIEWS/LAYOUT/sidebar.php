035fb34b4ce6f26ad1bd4ca2a73ee4d0121d0513
Kevin Cerón <kevin10595@hotmail.com>
2021-06-14 02:09:31 -0600

    Update sidebar.php

--- VIEWS/LAYOUT/sidebar.php
+++ VIEWS/LAYOUT/sidebar.php
@@ -1,19 +1,41 @@
 <!-- BARRA LATERAL -->
 <aside id="lateral">
     <div id="login" class="block_aside">
-        <h3>Entrar a la web</h3>
-        <form action="#" method="post">
-            <label for="email">Email</label>
-            <input type="text" name="email" id="">
-            <label for="password">Contraseña</label>
-            <input type="password" name="password" id="">
-            <input type="submit" value="Enviar  ">
-        </form>
+
+        <?php if (!isset($_SESSION['identity'])) : ?>
+
+
+
+
+
+            <h3>Entrar a la web</h3>
+            <form action="<?= base_url ?>usuario/login" method="post">
+                <label for="email">Email</label>
+                <input type="text" name="email" id="">
+                <label for="password">Contraseña</label>
+                <input type="password" name="password" id="">
+                <input type="submit" value="Enviar  ">
+            </form>
+        <?php else : ?>
+            <h3><?= $_SESSION['identity']->nombre; ?> <?= $_SESSION['identity']->apellidos; ?></h3>
+        <?php endif; ?>
         <ul>
-            <li><a href="#">Mis pedidos</a></li>
-            <li><a href="#">Gestionar pedidos</a></li>
-            <li><a href="#">Gestionar categoriass</a></li>
+
+            <?php if (isset($_SESSION['admin'])) : ?>
+                <li><a href="#">Gestionar categorias</a></li>
+                <li><a href="#">Gestionar productos</a></li>
+                <li><a href="#">Gestionar pedidos</a></li>
+            <?php endif; ?>
+
+
+            <?php if (isset($_SESSION['identity'])) : ?>
+                <li><a href="#">Mis pedidos</a></li>
+                <li><a href="<?= base_url ?>/usuario/logout">cerrar sesión</a></li>
+            <?php endif; ?>
+
         </ul>
+
+
     </div>
 </aside>
 

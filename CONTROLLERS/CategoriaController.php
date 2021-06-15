04df652306b9023d2b07d59d97ac1de94813e57d
Kevin Cerón <ceronkevin8882@gmail.com>
2021-06-14 20:55:26 -0600

    Update CategoriaController.php

--- CONTROLLERS/CategoriaController.php
+++ CONTROLLERS/CategoriaController.php
@@ -1,8 +1,36 @@
 <?php
 
+require_once 'MODELS/categoria.php';
+
 class CategoriaController{
     
     public function index(){
-        echo "Controlador categoria, accion index";
+        Utils::isAdmin();
+        $categoria = new Categoria();
+        $categorias = $categoria->listarCategorias();
+        require_once 'VIEWS/CATEGORIA/index.php';
     }
+
+    public function crear(){
+        Utils::isAdmin();
+        require_once 'VIEWS/CATEGORIA/crear.php';       
+    }
+
+    public function save(){
+
+        Utils::isAdmin();
+
+        if (isset($_POST) && isset($_POST['nombre'])) {
+            
+
+        //guardar categoria
+        $categoria= new Categoria();
+        $categoria->setNombre($_POST['nombre']);
+        $categoria->save();
+        header("Location:".base_url."categoria/index");
+
+        }
+    }
+
+
 }

5efba138c9977bf73026d8261c75f629ec38e3ce
Kevin Cerón <ceronkevin8882@gmail.com>
2021-06-14 20:55:29 -0600

    Update utils.php

--- HELPERS/utils.php
+++ HELPERS/utils.php
@@ -12,4 +12,21 @@ Class Utils{
         return $sessionName;
 
     }
+
+    public static function isAdmin(){
+        //En caso de que no exista la sesion de usuario
+        if (!isset($_SESSION['admin'])) {
+            header("Location:".base_url);
+        }
+        else{
+            return true;
+        }
+    }
+
+    public static function showCategorias(){
+        require_once 'MODELS/categoria.php';
+        $categoria = new Categoria();
+        $categorias = $categoria->listarCategorias();
+        return $categorias;
+    }
 }

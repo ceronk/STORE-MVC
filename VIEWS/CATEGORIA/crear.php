26e5a3e48749b2940b8f4fd4e52b2461f9495f26
Kevin Cerón <ceronkevin8882@gmail.com>
2021-06-14 20:55:36 -0600

    Create crear.php

---
+++ VIEWS/CATEGORIA/crear.php
@@ -0,0 +1,8 @@
+<h1>Crear Categoria</h1>
+
+<form action="<?= base_url ?>categoria/save" method="post">
+
+    <label for="">Nombre Categoria</label>
+    <input type="text" name="nombre" id="" required>
+    <input type="submit" value="Crear">
+</form>

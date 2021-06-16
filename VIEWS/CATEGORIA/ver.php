e26067522c5315f2d01882a539090c3676a26e45
Kevin Cerón <ceronkevin8882@gmail.com>
2021-06-16 00:47:21 -0600

    Update ver.php

--- VIEWS/CATEGORIA/ver.php
+++ VIEWS/CATEGORIA/ver.php
@@ -10,20 +10,24 @@
     <p>No hay productos para mostrar!</p>
 <?php else : ?>
 
-    <?php while ($print = $prdcts->fetch_object()) : ?>
+    <?php while($pro = $prdcts->fetch_object()):?>
 
-        <div class="product">
-            <?php if (isset($print->imagen) != null) : ?>
-                <img src="<?= base_url ?>/uploads/images/<?= $print->imagen; ?>" alt="">
-            <?php else : ?>
-                <img src="<?= base_url ?>/uploads/images/camiseta.png" alt="">
-            <?php endif; ?>
-            <h2><?= $print->nombre; ?></h2>
-            <p><?= $print->precio; ?></p>
-            <a href="<?= base_url ?>" class="button">Comprar</a>
-        </div>
 
-    <?php endwhile; ?>
+<div class="product">
+ <a href="<?=base_url?>producto/ver&id=<?=$pro->id?>">
+    <?php if(isset($pro->imagen) != null):?>
+    <img src="<?=base_url?>/uploads/images/<?=$pro->imagen;?>" alt="">
+    <?php else:?>
+        <img src="<?=base_url?>/uploads/images/camiseta.png" alt="">
+    <?php endif;?>
+
+    <h2><?=$pro->nombre;?></h2>
+    </a>
+    <p>$<?=$pro->precio;?></p>
+    <a href="<?=base_url?>" class="button">Comprar</a>
+</div>
+
+<?php endwhile;?>
 
 
 <?php endif; ?>

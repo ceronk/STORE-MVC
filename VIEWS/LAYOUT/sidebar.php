f0f6a6a4501abc6b2042b0d9c52ef969fd2633a2
Kevin Cerón <ceronkevin8882@gmail.com>
2021-06-16 03:13:26 -0600

    Update sidebar.php

--- VIEWS/LAYOUT/sidebar.php
+++ VIEWS/LAYOUT/sidebar.php
@@ -1,12 +1,20 @@
 <!-- BARRA LATERAL -->
 <aside id="lateral">
+
+    <div id="carrito" class="block_aside">
+    
+    <h3>Mi carrito</h3>
+    <ul>
+    <?php $item = Utils::statsCarrito();?>
+    <li><a href="<?=base_url?>carrito/index">Productos (<?=$item['count'];?>)</a></li>
+    <li><a href="<?=base_url?>carrito/index">Total: $<?=$item['total'];?></a></li>
+    <li><a href="<?=base_url?>carrito/index">Mi carrito</a></li>
+    </ul>
+    </div>
+
     <div id="login" class="block_aside">
 
         <?php if (!isset($_SESSION['identity'])) : ?>
-
-
-
-
 
             <h3>Entrar a la web</h3>
             <form action="<?= base_url ?>usuario/login" method="post">
@@ -27,7 +35,6 @@
                 <li><a href="<?=base_url?>">Gestionar pedidos</a></li>
             <?php endif; ?>
 
-
             <?php if (isset($_SESSION['identity'])) : ?>
                 <li><a href="#">Mis pedidos</a></li>
                 <li><a href="<?= base_url?>/usuario/logout">cerrar sesión</a></li>
@@ -36,12 +43,8 @@
             <?php endif; ?>
 
         </ul>
-
-
     </div>
 </aside>
-
-
 
 <!-- CONTENIDO CENTRAL  -->
 <div id="central">

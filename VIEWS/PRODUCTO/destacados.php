3f405c1e9ce6499201906e54e88665ce02bfbfb4
Kevin Cerón <ceronkevin8882@gmail.com>
2021-06-16 00:12:12 -0600

    Update destacados.php

--- VIEWS/PRODUCTO/destacados.php
+++ VIEWS/PRODUCTO/destacados.php
@@ -1,13 +1,19 @@
-<h1>Productos destacados</h1>
+<h1>Productos Aleatorios</h1>
+
+<?php while($pro = $prdcts->fetch_object()):?>
+
+
 <div class="product">
-    <img src="assets/img/camiseta.png" alt="">
-    <h2>Camiseta azul </h2>
-    <p>30 dolares</p>
-    <a href="#" class="button">Comprar</a>
+
+    <?php if(isset($pro->imagen) != null):?>
+    <img src="<?=base_url?>/uploads/images/<?=$pro->imagen;?>" alt="">
+    <?php else:?>
+        <img src="<?=base_url?>/uploads/images/camiseta.png" alt="">
+    <?php endif;?>
+
+    <h2><?=$pro->nombre;?></h2>
+    <p><?=$pro->precio;?></p>
+    <a href="<?=base_url?>" class="button">Comprar</a>
 </div>
-<div class="product">
-    <img src="assets/img/camiseta.png" alt="">
-    <h2>Camiseta azul </h2>
-    <p>30 dolares</p>
-    <a href="#" class="button">Comprar</a>
-</div>
+
+<?php endwhile;?>

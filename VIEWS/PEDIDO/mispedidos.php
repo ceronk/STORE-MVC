9629bbfe0a5514cfcd1794167e932697ebdc0fc5
Kevin Cerón <ceronkevin8882@gmail.com>
2021-06-18 02:00:08 -0600

    Update mispedidos.php

--- VIEWS/PEDIDO/mispedidos.php
+++ VIEWS/PEDIDO/mispedidos.php
@@ -1,10 +1,17 @@
-<h1>Mis pedidos</h1>
+
+<?php if(isset($gestion) && $gestion == true):?>
+    <h1>Gestión Pedidos | Panel Administración</h1>
+<?php else:?>
+    <h1>Mis pedidos</h1>
+<?php endif;?>
+
 
 <table>
     <tr>
         <th>Numero Pedido</th>
         <th>Coste</th>
         <th>Fecha</th>
+        <th>Estado Pedido</th>
     </tr>
 
     <?php //var_dump($pedidosData); die();?>
@@ -15,7 +22,8 @@
         <tr>
             <td><a href="<?=base_url?>pedido/detalle&id=<?= $items->id; ?>"><?= $items->id; ?></a></td>
             <td>$<?= $items->coste; ?></td>
-            <td><?= $items->fecha; ?></td>            
+            <td><?= $items->fecha; ?></td>
+            <td><?= $items->estado; ?></td>            
         </tr>
     <?php endwhile; ?>
     <?php endif;?>

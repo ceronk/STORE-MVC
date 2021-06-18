6f6d87e34cb90df0179500d182a91543f4777a33
Kevin Cerón <ceronkevin8882@gmail.com>
2021-06-18 01:28:12 -0600

    Update utils.php

--- HELPERS/utils.php
+++ HELPERS/utils.php
@@ -1,36 +1,48 @@
 <?php
 
 
-Class Utils{
+class Utils
+{
 
-    public static function deleteSession($sessionName){
+    public static function deleteSession($sessionName)
+    {
         if (isset($_SESSION[$sessionName])) {
             $_SESSION[$sessionName] = null;
             unset($_SESSION[$sessionName]);
         }
 
         return $sessionName;
-
     }
 
-    public static function isAdmin(){
+    public static function isAdmin()
+    {
         //En caso de que no exista la sesion de usuario
         if (!isset($_SESSION['admin'])) {
-            header("Location:".base_url);
-        }
-        else{
+            header("Location:" . base_url);
+        } else {
             return true;
         }
     }
 
-    public static function showCategorias(){
+    public static function isIdentity()
+    {
+        if (!isset($_SESSION['identity'])) {
+            header("Location:" . base_url);
+        } else {
+            return true;
+        }
+    }
+
+    public static function showCategorias()
+    {
         require_once 'MODELS/categoria.php';
         $categoria = new Categoria();
         $categorias = $categoria->listarCategorias();
         return $categorias;
     }
 
-    public static function statsCarrito(){
+    public static function statsCarrito()
+    {
         $stats = array(
             'count' => 0,
             'total' => 0
@@ -38,7 +50,7 @@ Class Utils{
         if (isset($_SESSION['carrito'])) {
             $stats['count'] = count($_SESSION['carrito']);
 
-            foreach($_SESSION['carrito'] as $productCarrito){
+            foreach ($_SESSION['carrito'] as $productCarrito) {
                 $stats['total'] += $productCarrito['precio'] * $productCarrito['unidades'];
             }
         }

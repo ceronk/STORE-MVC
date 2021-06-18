71d5737dda195e927f3bfdf26f7c14e739e299c8
Kevin Cerón <ceronkevin8882@gmail.com>
2021-06-18 03:00:33 -0600

    Update detalle.php

--- VIEWS/PEDIDO/detalle.php
+++ VIEWS/PEDIDO/detalle.php
@@ -3,15 +3,38 @@
 
 <?php if (isset($finalPedido)) : ?>
 
+    <?php if ($_SESSION['admin']) : ?>
+        <h3>Cambiar estado | Panel Administración</h3>
+        <hr>
+        <form action="<?= base_url ?>pedido/estado" method="post">
+
+            <input type="hidden" name="idPedido" value="<?= $finalPedido->id; ?>">
+
+
+            <label for="">Estado Pedido</label>
+            <select name="estado" id="">
+                <option value="confirm" <?= $finalPedido->estado == 'confirm' ? 'selected' : ''; ?>>Pendiente</option>
+                <option value="preparation" <?= $finalPedido->estado == 'preparation' ? 'selected' : ''; ?>>En Preparación</option>
+                <option value="ready" <?= $finalPedido->estado == 'ready' ? 'selected' : ''; ?>>Preparado para el enviar</option>
+                <option value="sended" <?= $finalPedido->estado == 'sended' ? 'selected' : ''; ?>>Enviado</option>
+            </select>
+
+
+            <input type="submit" value="Cambiar estado">
+        </form>
+        <br>
+    <?php endif; ?>
+
     <h3>Direccion de envio: </h3>
     <hr>
     <p>Ciudad: <?= $finalPedido->localidad; ?></p>
     <p>Municipio: <?= $finalPedido->provincia; ?></p>
     <p>Direccion: <?= $finalPedido->direccion; ?></p>
-<hr>
-<br>
+    <hr>
+    <br>
     <h3>Datos del pedido: </h3>
     <hr>
+    <p>Estado: <?= Utils::showStatus($finalPedido->estado); ?></p>
     <p> Numero de pedido: <?= $finalPedido->id; ?></p>
     <p>Total a Pagar: <?= $finalPedido->coste; ?></p>
     <br>
